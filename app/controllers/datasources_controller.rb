ff035bfddc0baf2d2c13f6b4a90b2fc9c9ee0333
Troy Davis <troy@sevenscale.com>
2010-10-09 23:31:51 -0700

    render nothing from update_fires action

--- app/controllers/datasources_controller.rb
+++ app/controllers/datasources_controller.rb
@@ -1,6 +1,6 @@
 class DatasourcesController < ApplicationController
   def update_fires
     FireDispatchFetcher.new.get_fire_data
-    render :status => :ok
+    render :nothing => true, :status => :ok
   end
 end
