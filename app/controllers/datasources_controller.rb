2e3647e5d079b7421c38cdc256bd05f29ef32b39
Troy Davis <troy@sevenscale.com>
2011-01-06 16:19:21 -0800

    move import processing to be on demand

--- app/controllers/datasources_controller.rb
+++ app/controllers/datasources_controller.rb
@@ -3,4 +3,14 @@ class DatasourcesController < ApplicationController
     FireDispatchFetcher.new.get_fire_data
     render :nothing => true, :status => :ok
   end
+
+  def update_police
+    police_report_fetcher = PoliceReportFetcher.new  
+    if Time.now.hour % 3 == 0
+      police_report_fetcher.get_all_crime_data(1.day.ago.strftime('%m/%d/%Y'), Time.now.strftime('%m/%d/%Y'))
+    end
+
+    police_report_fetcher.get_all_incident_data(2, 0)
+    render :nothing => true, :status => :ok
+  end
 end
